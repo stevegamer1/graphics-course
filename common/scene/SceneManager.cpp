17d94e38890149566ef725282190e5c36bbf7957
stevegamer1 <kornei.sd@phystech.edu>
2025-06-19 00:24:05 +0300

    fix material warning

--- common/scene/SceneManager.cpp
+++ common/scene/SceneManager.cpp
@@ -205,6 +205,7 @@ SceneManager::ProcessedMeshes SceneManager::processMeshes(const tinygltf::Model&
         .vertexOffset = static_cast<std::uint32_t>(result.vertices.size()),
         .indexOffset = static_cast<std::uint32_t>(result.indices.size()),
         .indexCount = static_cast<std::uint32_t>(accessors[0]->count),
+        .material = Material::none()
       });
       const std::size_t vertexCount = accessors[1]->count;
       std::array ptrs{
